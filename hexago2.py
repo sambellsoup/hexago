5dd37750c1c27501b1dcdfb78d0e682dbdd12c73
sambellsoup <sambellsoup@gmail.com>
2020-02-08 23:27:31 -0500

    removing collision circle from player

--- hexago2.py
+++ hexago2.py
@@ -32,7 +32,7 @@ class Player(pygame.sprite.Sprite):
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.radius = 350
-        pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
